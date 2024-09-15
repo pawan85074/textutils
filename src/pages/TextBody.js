3e59492c86e4cfd96a18394a327b461b4bcb0cbd
pawan85074 <pawanraj85074@gmail.com>
2024-09-15 23:15:13 +0530

    some feature add such as total word count and total char and reading time

--- src/pages/TextBody.js
+++ src/pages/TextBody.js
@@ -20,13 +20,17 @@ export default function TextBody() {
   return (
     <div>   
         <div className="mb-3 my-5">
-        <h1 className="bg-danger text-light">Enter The Sentence And Analyze It. </h1>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="17" value={text} onChange={handleInp}/>
+        <h1 className="bg-danger text-light text-center">Enter The Sentence And Analyze It. </h1>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="11" value={text} onChange={handleInp}/>
         </div>
         <div className='text-center'>
         <button type="button" className="btn btn-primary mx-2" onClick={handleUpperCase}>UpperCase</button>
         <button type="button" className="btn btn-primary mx-2" onClick={handleLowerCase}>LowerCase</button>
         <button type="button" className="btn btn-primary mx-2" onClick={handleClear}>clear</button>
+        </div>
+        <div className='container my-3'>
+         <h2 >Your Text Summary</h2>
+         <p className='text-light font-weight-bold pb-3'>Total words : {text.split(" ").length} <br/> Total Characters : {text.length} <br/>Read Time : {0.08*text.split(" ").length} min</p>
         </div>
     </div>
   )
